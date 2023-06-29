f646695ea66e08881b5d02a2cb0cc80e1c6735b1
Raphael Berger <raphaelmb@gmail.com>
2023-06-29 15:49:56 -0300

    add logout and user auth

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -62,7 +62,12 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
