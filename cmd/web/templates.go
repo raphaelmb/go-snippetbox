a1ab18a3869c0be3def97d123f31d4214e7f93b1
Raphael Berger <raphaelmb@gmail.com>
2023-07-01 17:33:50 -0300

    add embedfs and auth check

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"github.com/raphaelmb/go-snippetbox/internal/models"
+	"github.com/raphaelmb/go-snippetbox/ui"
 )
 
 type templateData struct {
@@ -28,24 +30,24 @@ var functions = template.FuncMap{
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl.html")
 	if err != nil {
 		return nil, err
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
+
+		patterns := []string{
+			"html/base.tmpl.html",
+			"html/partials/*.tmpl.html",
+			page,
+		}
+
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
+
 		cache[name] = ts
 	}
 	return cache, nil
